7119de3f14e5c42ea8256e056e19d87d5e14c334
Renchester Ramos <renchesterjramos@gmail.com>
2023-09-01 05:54:50 +0800

    Add functions within UserService

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { USER_REPOSITORY } from 'src/core/constants';
+import { User } from './users.entity';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @Inject(USER_REPOSITORY) private readonly userRepository: typeof User,
+  ) {}
+
+  async create(user: UserDto): Promise<User> {
+    return await this.userRepository.create<User>(user);
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOne<User>({ where: { email } });
+  }
+
+  async findOneById(id: number): Promise<User> {
+    return await this.userRepository.findOne<User>({ where: { id } });
+  }
+}
