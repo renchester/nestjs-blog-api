4aa4ddb33306fa86dbb576e00dca46c28856c340
Renchester Ramos <renchesterjramos@gmail.com>
2023-09-01 05:32:44 +0800

    Add user repository constants

---
+++ src/modules/users/users.providers.ts
@@ -0,0 +1,9 @@
+import { User } from './users.entity';
+import { USER_REPOSITORY } from '../../core/constants';
+
+export const usersProviders = [
+  {
+    provide: USER_REPOSITORY,
+    useValue: User,
+  },
+];
