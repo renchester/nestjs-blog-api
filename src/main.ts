c8f057955a76d30f68e32eb7035ae518a0932a84
Renchester Ramos <renchesterjramos@gmail.com>
2023-09-02 12:52:51 +0800

    Create validator input pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Global Prefix
   app.setGlobalPrefix('api/v1');
+
+  // Handle all user input validation globally
+  app.useGlobalPipes(new ValidateInputPipe());
 
   await app.listen(3000);
 }
