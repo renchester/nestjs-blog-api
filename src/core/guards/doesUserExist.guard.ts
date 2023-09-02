c2cdca0ab26081d04bdff30fd4a7420e5e894760
Renchester Ramos <renchesterjramos@gmail.com>
2023-09-02 13:45:42 +0800

    Add guard to check if user exists

---
+++ src/core/guards/doesUserExist.guard.ts
@@ -0,0 +1,32 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { UsersService } from 'src/modules/users/users.service';
+
+@Injectable()
+export class DoesUserExist implements CanActivate {
+  constructor(private readonly userService: UsersService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  async validateRequest(request: Request) {
+    const userExists = await this.userService.findOneByEmail(
+      request.body.email,
+    );
+    if (userExists) {
+      throw new ForbiddenException('This email already exists');
+    }
+
+    return true;
+  }
+}
