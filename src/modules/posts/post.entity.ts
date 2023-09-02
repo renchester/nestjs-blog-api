696c5e35c45b6efe6d61298441ac5825aa6baa2a
Renchester Ramos <renchesterjramos@gmail.com>
2023-09-02 13:49:17 +0800

    Create Post entity and provider modules

---
+++ src/modules/posts/post.entity.ts
@@ -0,0 +1,34 @@
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from '../users/users.entity';
+
+@Table
+export class Post extends Model<Post> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title: string;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  body: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  userId: string;
+
+  @BelongsTo(() => User)
+  user: User;
+}
